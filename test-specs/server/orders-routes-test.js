ffa90d0759299016a525ac505d14a9ca88b59831
emtseng <etseng42@gmail.com>
2017-04-20 12:23:57 -0400

    feat: creates routes for Order model and working test suite

--- test-specs/server/orders-routes-test.js
+++ test-specs/server/orders-routes-test.js
@@ -9,7 +9,7 @@ const request = require('supertest')
 
 /* global describe it before beforeEach after afterEach priceAtOrderTime */
 
-describe('Products API', () => {
+describe('Orders API', () => {
   before('Await database sync', () => db.didSync)
   after('Sync the tables', () => db.sync({force: true}))
 
@@ -25,19 +25,31 @@ describe('Products API', () => {
     greenTrunks = {
       name: 'Green Trunks',
       price: 100000,
+    },
+    internationalOrder = {
+      instructions: 'I am a dummy order',
+      shipping: 'International'
+    },
+    twoDayOrder = {
+      instructions: 'I\'m a dummy order too!',
+      shipping: 'Two-Day'
+    },
+    thirdOrder = {
+      instructions: 'I was POSTed',
+      shipping: 'Two-Day'
     }
 
   beforeEach('Add dummy data to tables', () => {
-    // Use promise chaining to ensure blueShoes is created before redShoes.
-    return Product.create(blueShoes)
-    .then(() => Product.create(redShoes))
+    // Use promise chaining to ensure internationalOrder is created before twoDayOrder.
+    return Order.create(internationalOrder)
+    .then(() => Order.create(twoDayOrder))
   })
 
-  describe('/api/products', () => {
+  describe('/api/orders', () => {
 
-    it('can GET all products', done => {
+    it('can GET all orders', done => {
       request(app)
-      .get('/api/products/')
+      .get('/api/orders/')
       .then(res => {
         expect(res.body[0].id).to.equal(1)
         expect(res.body.length).to.equal(2)
@@ -47,12 +59,12 @@ describe('Products API', () => {
       .catch(done)
     })
 
-    it('can POST a new product', done => {
+    it('can POST a new order', done => {
       request(app)
-      .post('/api/products/')
-      .send(greenTrunks)
+      .post('/api/orders/')
+      .send(thirdOrder)
       .then(res => {
-        expect(res.body.name).to.equal('Green Trunks')
+        expect(res.body.instructions).to.equal('I was POSTed')
         expect(res.status).to.equal(201)
         done()
       })
@@ -62,25 +74,25 @@ describe('Products API', () => {
 
   describe('/api/products/:id', () => {
 
-    it('can GET a specific product by id', done => {
+    it('can GET a specific order by id', done => {
       request(app)
-      .get('/api/products/1')
+      .get('/api/orders/1')
       .then(res => {
-        expect(res.body.name).to.equal('Blue Suede Shoes')
-        expect(res.body.available).to.be.true
+        expect(res.body.instructions).to.equal('I am a dummy order')
+        expect(res.body.shipping).to.equal('International')
         done()
       })
       .catch(done)
     })
 
-    it('can PUT to update a specific product...', done => {
+    it('can PUT to update a specific order...', done => {
       request(app)
-      .put('/api/products/1')
+      .put('/api/orders/1')
       .send({
-        name: 'Blue Suede Pants'
+        shipping: 'Two-Day'
       })
       .then(res => {
-        expect(res.body.name).to.equal('Blue Suede Pants')
+        expect(res.body.shipping).to.equal('Two-Day')
         done()
       })
       .catch(done)
@@ -88,12 +100,12 @@ describe('Products API', () => {
 
     it('...and it can PUT to update that product again', done => {
       request(app)
-      .put('/api/products/1')
+      .put('/api/orders/1')
       .send({
-        name: 'Blue Suede Shoes'
+        shipping: 'International'
       })
       .then(res => {
-        expect(res.body.name).to.equal('Blue Suede Shoes')
+        expect(res.body.shipping).to.equal('International')
         done()
       })
       .catch(done)
