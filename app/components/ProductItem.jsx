4470c9783a1343f33a01ba1001806421ffa1022c
Nathan Anecone <nanecone@gmail.com>
2017-04-21 17:17:16 -0400

    fix bugs with ProductItem component

--- app/components/ProductItem.jsx
+++ app/components/ProductItem.jsx
@@ -4,51 +4,56 @@ import {Link} from 'react-router'
 import removeProduct from './dispatchers/removeProduct'
 
 
-// we don't have api routes???
-
-
-
 class ProductItem extends React.Component {
   constructor(props){
     super(props)
-    this.removeProduct = this.removeProduct.bind(this)
+    this.renderRemoveProduct = this.renderRemoveProduct.bind(this)
+  }
+
+  render(){
+    const product = this.props.product
+      return(
+        <div className="list-group-item min-content product-item">
+            <div className="media">
+              <div className="media-left media-middle icon-container">
+                <img className="media-object img-circle" src={product.photo[0]} />
+              </div>
+              <Link
+                className="media-body"
+                activeClassName="active"
+                to={`/products/${product.id}`}>
+                <h4 className="media-heading tucked">
+                  <span placeholder="A product">{product.name}</span>
+                </h4>
+                <h5 className="tucked">
+                  <span>{product.price}</span>
+                </h5>
+              </Link>
+            </div>
+
+            {this.props.isAdmin ? this.renderRemoveProduct() : null }
+
+      </div>
+    )
+  }
+
+  renderRemoveProduct() {
+    return (
+      <div className="media-right media-middle">
+        <button
+            className="btn btn-default"
+            onClick={removeProduct(product.id)}>
+          <span className="glyphicon glyphicon-remove" />
+        </button>
+      </div>
+    )
   }
 
 
-render(){
-  const {product} = props.product
-  return(
-    <div className="list-group-item min-content user-item">
-        <div className="media">
-          <div className="media-left media-middle icon-container">
-            <img className="media-object img-circle" src={product.photo} />
-          </div>
-          <Link
-            className="media-body"
-            activeClassName="active"
-            to={`/products/${product.id}`}>
-            <h4 className="media-heading tucked">
-              <span placeholder="A product">{product.title}</span>
-            </h4>
-            <h5 className="tucked">
-              <span>{product.price}</span>
-            </h5>
-          </Link>
 
-          <div className="media-right media-middle">
-            <button
-                className="btn btn-default"
-                onClick={this.renderRemovePanel}>
-              <span className="glyphicon glyphicon-remove" />
-            </button>
-          </div>
-          {this.props.isAdmin ? this.renderRemovePanel : null}
-        </div>
-  </div>
-  )
-}
-  renderRemovePanel(event){
-    event.stopPropagation()
-    removeProduct(product.id)
-  }
+  // event.stopPropagation()
+  // removeProduct(product.id)
+
+
+
 }
