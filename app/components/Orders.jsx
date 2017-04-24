09d8ba05b95d5b827f202f5efe89572876963d71
tina <tina.lam516@gmail.com>
2017-04-24 10:05:57 -0400

    update: some orders table additions

--- app/components/Orders.jsx
+++ app/components/Orders.jsx
@@ -5,34 +5,30 @@ const Orders = (props) => (
   <table className="table table-striped table-hover">
     <thead>
       <tr>
-        <th>#</th>
+        <th>Order Date</th>
         <th>Order Number</th>
-        <th>Shipping Method</th>
-        <th>Date</th>
-        <th>Status</th>
+        <th>Shipping Date</th>
+        <th>Tracking Number</th>
       </tr>
     </thead>
     <tbody>
      <tr>
-       <td>1</td>
-       <td>123456</td>
-       <td>Two-Day</td>
        <td>3/26/2017</td>
-       <td>Delivered</td>
+       <td>3808439075</td>
+       <td>4/1/2017</td>
+       <td>1Z849EW10351211223</td>
      </tr>
      <tr>
-       <td>2</td>
-       <td>123678</td>
-       <td>Standard</td>
        <td>4/21/2017</td>
-       <td>Shipped</td>
+       <td>4610684904</td>
+       <td>4/24/2017</td>
+       <td>1ZA4562R0360215646</td>
      </tr>
      <tr>
-       <td>3</td>
-       <td>123790</td>
-       <td>International</td>
        <td>4/24/2017</td>
-       <td>Processed</td>
+       <td>3405265668</td>
+       <td>Processing</td>
+       <td>1ZA4562R0332128114</td>
      </tr>
    </tbody>
  </table>
