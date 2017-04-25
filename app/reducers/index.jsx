4848827f16582c16baa82d7dda7f11c6c51b9672
Keisuke Kido <kskkido@gmail.com>
2017-04-25 11:35:40 -0400

    create: user reducer, pendingOrder reducer, shopping cart component

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   products: require('./products-reducer').default,
-  user: require('./users-reducer-temp').default,
+  user: require('./users-reduce').default,
   // selectedProduct: require('./selectedProduct-reducer').default,
   auth: require('./auth-reducer').default,
   cart: require('./pendingOrder-reducer-temp').default,
