4296f181010d87dd46ea6c16c984b9dc4f95bf48
emtseng <etseng42@gmail.com>
2017-04-25 14:51:35 -0400

    fix: resolves merge conflicts

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -16,29 +16,31 @@ import User from './components/user/User'
 import Orders from './components/user/Orders'
 import Addresses from './components/user/Addresses'
 import EditAddress from './components/user/EditAddress'
+import Cart from './components/shoppingCart'
 
 import { fetchProducts } from './reducers/products-reducer'
 
 /* ------ COMPONENT ------ */
 
 const Routes = ({ fetchInitialData }) => (
-  <Router history={ browserHistory }>
-      <Route path="/" component={ App } onEnter={ fetchInitialData } >
+  <Router history={browserHistory}>
+    <Route path="/" component={App} onEnter={fetchInitialData} >
       <IndexRedirect to="/home" />
-      <Route path="/home" component={ Home } />
-      <Route path="/authenticate" component={ Auth } />
-      <Route path="/products" component={ Products } />
-      <Route path='/products/:id' component = {SingleProduct} />
-      <Route path="/users/:userId" component={ User }>
-        <Route path="orders" component={ Orders } />
-        <Route path="reviews" component={ User } />
-        <Route path="addresses" component={ Addresses } />
-        <Route path="addresses/editAddress" component={ EditAddress } />
-        <Route path="settings" component={ User } />
+      <Route path="/home" component={Home} />
+      <Route path="/authenticate" component={Auth} />
+      <Route path="/products" component={Products} />
+      <Route path='/products/:id' component={SingleProduct} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/users/:userId" component={User}>
+        <Route path="orders" component={Orders} />
+        <Route path="reviews" component={User} />
+        <Route path="addresses" component={Addresses} />
+        <Route path="addresses/editAddress" component={EditAddress} />
+        <Route path="settings" component={User} />
       </Route>
-      </Route>
-      <Route path='*' component={ NotFound } />
-    </Router>
+    </Route>
+    <Route path='*' component={NotFound} />
+  </Router>
 )
 
 /* ------ CONTAINER ------ */
